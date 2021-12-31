1a8aca4b74e6f409ae3d6fb200b44c4c6bfa2a58
dppatnai <devi.patnaik@oracle.com>
2021-12-31 22:12:19 +0530

    mail notification3 configured in jenkins

--- src/test/java/com/javadev/jenkins/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/javadev/jenkins/api/SpringJenkinsApplicationTests.java
@@ -18,7 +18,7 @@ class SpringJenkinsApplicationTests {
 		
 		logger.info("Test Case Executing Second Log Statement....");
 		
-		logger.info("Email Notification Test2....");
+		logger.info("Email Notification Test3....");
 		
 		assertEquals(true, true);
 	}
